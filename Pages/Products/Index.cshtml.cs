452c13a4a9aad7e7c7391f1da76041b3ac079805
Anas KBL <kabila.anas1@gmail.com>
2023-10-08 20:04:05 +0100

    Added product search and category filter

--- Pages/Products/Index.cshtml.cs
+++ Pages/Products/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Shoping.Data;
 using Shoping.Models;
@@ -21,14 +22,61 @@ namespace Shoping.Pages.Products
 
         public IList<Product> Product { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+        public SelectList? Categories { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? ProductCategory { get; set; }
+
         public async Task OnGetAsync()
         {
             ViewData["header"] = "Products";
-            if (_context.Product != null)
+
+            //get all categories from Categories table without Product table
+            IQueryable<string> categoryQuery = from m in _context.Category
+                                               select m.Name;
+
+            var products = from m in _context.Product.Include(p => p.Category)
+                           select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
             {
-                Product = await _context.Product
-                .Include(p => p.Category).ToListAsync();
+                products = products.Where(p => p.Name.Contains(SearchString));
             }
+
+            if (!string.IsNullOrEmpty(ProductCategory))
+            {
+                products = products.Where(p => p.Category.Name == ProductCategory);
+            }
+            Categories = new SelectList(await categoryQuery.Distinct().ToListAsync());
+            Product = await products.ToListAsync();
+
+            /*if (_context.Product != null)
+            {
+                if (string.IsNullOrEmpty(SearchString) && string.IsNullOrEmpty(ProductCategory))
+                {
+                    Product = await _context.Product
+                    .Include(p => p.Category).ToListAsync();
+                }
+
+                if (!string.IsNullOrEmpty(SearchString))
+                {
+                    Product = await _context.Product
+                    .Include(p => p.Category)
+                    .Where(p => p.Name.Contains(SearchString))
+                    .ToListAsync();
+                }
+
+                if (!string.IsNullOrEmpty(ProductCategory))
+                {
+                    Product = await _context.Product
+                    .Include(p => p.Category)
+                    .Where(p => p.Category.Name == ProductCategory)
+                    .ToListAsync();
+                }
+
+
+            }*/
         }
     }
 }
